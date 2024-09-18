3f8fb7ed5d7a6bae3702249c23f35e40186a0e92
nishi <nishi.in.japan@gmail.com>
2024-09-18 22:16:19 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/vue.svg" class="logo vue" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
